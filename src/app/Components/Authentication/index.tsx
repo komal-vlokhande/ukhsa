eaa7753521142c4954281c3eb66747b0be1d9bb7
komal.lokhande <komal.lokhande@accenture.com>
2022-12-14 23:04:15 +0000

    feature: create auth component and route

--- src/app/Components/Authentication/index.tsx
+++ src/app/Components/Authentication/index.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { auth } from '../../store/auth/models/auth';
+import { AppState } from '../../store/rootStore';
+import { AppActions } from '../../store/models/actions';
+import { getAuthenticationDetails } from '../../store/auth/authAction';
 
-export const Authentication = () =>{
+interface Props {} 
+
+interface LinkStateProps {
+    authDetails: auth;
+  }
+
+  interface LinkDispatchProps {
+    getAuthenticationDetails: () => void;
+  }
+  
+  type LinkProps = Props & LinkStateProps & LinkDispatchProps;
+
+const mapStateToProps = (state: AppState): LinkStateProps => ({
+    authDetails: state.authReducer.auth,
+  });
+
+  const mapDispatchToProps = (
+    dispatch: ThunkDispatch<AppState, {}, AppActions>
+  ) => ({
+    getAuthenticationDetails: bindActionCreators(getAuthenticationDetails, dispatch),
+  });
+
+  class Authentication extends Component<LinkProps> {
+    componentDidMount() {
+      debugger;
+      //send req data to getAuthenticationDetails
+      this.props.getAuthenticationDetails();
+    }
+    render() {
+        const { authDetails } = this.props;
+        console.log(authDetails)
     return(
         <div>
-            Authentication
+            Authentications
         </div>
     )
+    }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Authentication);
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,12 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import '../styles.scss';
-import {App as Authentication} from './Containers/App'
-
+import Authentication from './Components/Authentication'
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/auth" element={<Authentication />} />
-      
-      </Routes>
-    </div>
-  );
+  return 
+    <Routes>
+      <Route path="/auth" element={<Authentication />} />
+    </Routes> 
 }
 
 export default App;
