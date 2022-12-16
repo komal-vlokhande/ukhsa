59f2378bb9f157042d551e24d4d0ac2ab4b79100
mfidow <muna.fidow@ukhsa.gov.uk>
2022-12-16 20:29:47 +0000

    tested welcome component

---
+++ src/app/Components/Welcome Page/index.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import { describe, expect, test } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+
+import { WelcomePage } from ".";
+describe("WelcomePage", () => {
+  it("displays the heading with the correct text", () => {
+    const { getByText } = render(<WelcomePage />);
+    const heading = getByText("Welcome to the form");
+    expect(heading).toBeInTheDocument();
+  });
+  it("renders paragraph with correct text", () => {
+    const { getByText } = render(<WelcomePage />);
+    const paragraph = getByText(/lorem ipsum/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+  it("renders the start button", () => {
+    const { getByText } = render(<WelcomePage />);
+    const button = getByText("Start now");
+    expect(button).toBeInTheDocument();
+  });
+  it("disables the start button", () => {
+    render(<WelcomePage />);
+    const button = screen.getByText("Start now");
+    expect(button).toBeDisabled();
+  });
+});
--- src/app/Components/Welcome Page/index.tsx
+++ src/app/Components/Welcome Page/index.tsx
@@ -1,6 +1,34 @@
-import React from "react";
-import { Button } from "govuk-react-jsx";
-export const WelcomePage = () => {
+import * as React from "react";
+//@ts-ignore
+// import { Button } from "govuk-react-jsx";
+// export class WelcomePage extends React.component {
+//   render(): JSX.Element {
+//     return (
+//       <>
+//         <div className="govuk-width-container">
+//           <main className="govuk-main-wrapper govuk-main-wrapper--l">
+//             <div className="govuk-grid-row">
+//               <div className="govuk-grid-column-two-thirds">
+//                 <h1 className="govuk-heading-xl">Welcome to the form</h1>
+//                 <p className="govuk-body-m">
+//                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+//                 </p>
+//               </div>
+//             </div>
+//             <button disabled>Start now </button>
+//             {/* <Button isStartButton disabled>
+//             Start now{" "}
+//           </Button> */}
+//           </main>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+// import React from "react";
+// //@ts-ignore
+// // import { Button } from "govuk-react-jsx";
+export const WelcomePage = (): JSX.Element => {
   return (
     <>
       <div className="govuk-width-container">
@@ -10,15 +38,13 @@ export const WelcomePage = () => {
               <h1 className="govuk-heading-xl">Welcome to the form</h1>
               <p className="govuk-body-m">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Expedita eveniet illum iste ducimus mollitia aut tenetur
-                nesciunt voluptas impedit possimus perferendis laudantium,
-                dolore distinctio! Accusantium ducimus placeat qui minus magnam!
               </p>
             </div>
           </div>
-          <Button isStartButton disabled>
+          <button disabled>Start now </button>
+          {/* <Button isStartButton disabled>
             Start now{" "}
-          </Button>
+          </Button> */}
         </main>
       </div>
     </>
