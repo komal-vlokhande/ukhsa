36937fca25b660c0c1cd93aa8f786f20310dd661
komal-vlokhande <komal.lokhande@ukhsa.gov.uk>
2022-12-16 09:54:50 +0000

    Added theme based approach

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,57 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, {createContext, useState} from "react";
+import { Route, Routes } from "react-router-dom"; 
+import { Template, Header, Footer, DateInput ,Select } from 'govuk-react-jsx';
 import '../styles.scss';
 import { Authentication } from './Components/Authentication'
+
+export const ThemeContext = createContext(null);
+
+function NoMatch() {
+  return <div>404 Not found</div>;
+}
+
 function App() {
+  const [theme,setTheme] = useState("--theme-light");
+
+  const toggleTheme = (e: { target: { value: any; }; }) => {
+    setTheme((curr)=>(curr === '--theme-light'? "--theme-dark" : "--theme-light"));
+  }
+
   return (
-    <Routes>
-      <Route path="/auth" element={<Authentication />} />
-    </Routes> )
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <div className="App" id={theme}>
+          <Header 
+          menuButtonLabel="Theme"   
+          containerClassName="govuk-header__container--full-width"
+          navigationClassName="govuk-header__navigation--end"/>
+            <div className="switch">
+              <Select
+                id="select-1"
+                items={[
+                  {
+                    children: 'Light Theme',
+                    value: '--theme-light'
+                  },
+                  {
+                    children: 'Dark Theme',
+                    value: '--theme-dark'
+                  }
+                ]}
+                name="select-1"
+                value={theme}
+                onChange={toggleTheme}
+              />
+            </div>
+            <div className="content">
+              <Routes>
+                <Route path="/auth" element={<Authentication />} />
+                <Route path="*" element={<NoMatch />} />
+              </Routes>
+            </div> 
+         <Footer />
+        </div>
+      </ThemeContext.Provider>
+    )
 
 }
 
