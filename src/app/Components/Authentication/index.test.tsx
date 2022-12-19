2e4d5c9d8ba61e7d3da8b487e96449adb939a5f5
mfidow <muna.fidow@ukhsa.gov.uk>
2022-12-19 17:34:16 +0000

    jest setup

--- src/app/Components/Authentication/index.test.tsx
+++ src/app/Components/Authentication/index.test.tsx
@@ -1,20 +1,21 @@
 import React from "react";
+
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import AuthPage from ".";
-describe("AuthPage", () => {
+import Authentication from ".";
+describe("Authentication", () => {
   it("displays the heading with the correct text", () => {
-    const { getByText } = render(<AuthPage />);
+    const { getByText } = render(<Authentication />);
     const heading = getByText(/date of birth/);
     expect(heading).toBeInTheDocument();
   });
   it("should render continue button", () => {
-    const { getByText } = render(<AuthPage />);
+    const { getByText } = render(<Authentication />);
     const continueButton = getByText("Continue");
     expect(continueButton).toBeInTheDocument();
   });
   it("should display error summary when error is present in state", async () => {
-    render(<AuthPage />);
+    render(<Authentication />);
     userEvent.click(screen.getByText("Continue"));
     const errorSummary = screen.getByRole("alert", {
       name: /there is a problem/i,
@@ -25,12 +26,12 @@ describe("AuthPage", () => {
   });
   describe("DateInput", () => {
     it("renders hint text", () => {
-      const { getByText } = render(<AuthPage />);
+      const { getByText } = render(<Authentication />);
       const hintText = getByText(/For example, 31 11 1980/i);
       expect(hintText).toBeInTheDocument();
     });
     it("renders input fields ", () => {
-      const { getByRole } = render(<AuthPage />);
+      const { getByRole } = render(<Authentication />);
       const dayField = getByRole("textbox", { name: /day/i });
       const monthField = getByRole("textbox", { name: /month/i });
       const yearField = getByRole("textbox", { name: /year/i });
@@ -40,14 +41,14 @@ describe("AuthPage", () => {
       expect(yearField).toBeInTheDocument();
     });
     it("displays error message /Please enter your date of birth/ if empty date field is submitted ", () => {
-      render(<AuthPage />);
+      render(<Authentication />);
       fireEvent.submit(document.querySelector("#dob"));
       expect(screen.getByRole("alert").textContent).toContain(
         "Please enter your date of birth"
       );
     });
     it("displays error message /Please input your date of birth in the correct format/ if invalid date field is submitted ", () => {
-      render(<AuthPage />);
+      render(<Authentication />);
       const dayField = screen.getByRole("textbox", { name: /day/i });
       const monthField = screen.getByRole("textbox", { name: /month/i });
       const yearField = screen.getByRole("textbox", { name: /year/i });
