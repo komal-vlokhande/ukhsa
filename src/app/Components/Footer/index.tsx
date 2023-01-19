4c9aab6f724b34cd4ef113e050a29f5de34174db
mfidow <muna.fidow@ukhsa.gov.uk>
2023-01-19 10:33:56 +0000

    updated index.tsx with refactored code

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -1,45 +1,28 @@
 import * as React from "react";
 import { Footer } from "govuk-react-jsx";
-
+const links = {
+  Help: "https://www.gov.uk/help",
+  Privacy: "https://www.gov.uk/help/privacy-notice",
+  Cookies: "https://www.gov.uk/help/cookies",
+  "Accessibility statement": "https://www.gov.uk/help/accessibility-statement",
+  Contact: "https://www.gov.uk/contact",
+  "Terms and conditions": "https://www.gov.uk/help/terms-conditions",
+  "Rhestr o Wasanaethau Cymraeg": "https://www.gov.uk/cymraeg",
+  "Government Digital Service":
+    "https://www.gov.uk/government/organisations/government-digital-service",
+};
+const mapLinks = (links) => {
+  return Object.entries(links).map(([children, href]) => {
+    return { children, href };
+  });
+};
 export class CustomFooter extends React.Component {
   render(): JSX.Element {
+    const footerLinks = mapLinks(links);
     return (
       <Footer
         meta={{
-          items: [
-            {
-              children: "Help",
-              href: "https://www.gov.uk/help",
-            },
-            {
-              children: "Privacy",
-              href: "https://www.gov.uk/help/privacy-notice",
-            },
-            {
-              children: "Cookies",
-              href: "https://www.gov.uk/help/cookies",
-            },
-            {
-              children: "Accessibility statement",
-              href: "https://www.gov.uk/help/accessibility-statement",
-            },
-            {
-              children: "Contact",
-              href: "https://www.gov.uk/contact",
-            },
-            {
-              children: "Terms and conditions",
-              href: "https://www.gov.uk/help/terms-conditions",
-            },
-            {
-              children: "Rhestr o Wasanaethau Cymraeg",
-              href: "https://www.gov.uk/cymraeg",
-            },
-            {
-              children: "Government Digital Service",
-              href: "https://www.gov.uk/government/organisations/government-digital-service",
-            },
-          ],
+          items: footerLinks,
         }}
       />
     );
