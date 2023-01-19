04d293444c006f5b6dfece09d41ded51d01c3b6c
mfidow <muna.fidow@ukhsa.gov.uk>
2023-01-19 10:26:45 +0000

    refactored code

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -31,44 +31,4 @@ export class CustomFooter extends React.Component {
       />
     );
   }
-  2;
-}
-{
-  /* <Footer
-meta={{
-  items: [
-    {
-      children: "Help",
-      href: "https://www.gov.uk/help",
-    },
-    {
-      children: "Privacy",
-      href: "https://www.gov.uk/help/privacy-notice",
-    },
-    {
-      children: "Cookies",
-      href: "https://www.gov.uk/help/cookies",
-    },
-    {
-      children: "Accessibility statement",
-      href: "https://www.gov.uk/help/accessibility-statement",
-    },
-    {
-      children: "Contact",
-      href: "https://www.gov.uk/contact",
-    },
-    {
-      children: "Terms and conditions",
-      href: "https://www.gov.uk/help/terms-conditions",
-    },
-    {
-      children: "Rhestr o Wasanaethau Cymraeg",
-      href: "https://www.gov.uk/cymraeg",
-    },
-    {
-      children: "Government Digital Service",
-      href: "https://www.gov.uk/government/organisations/government-digital-service",
-    },
-  ],
-}} */
 }
